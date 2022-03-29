35ad610f0a1b51219d47db6f7ef901529a53e534
Calvin Cheng <calvincheng919@gmail.com>
2022-03-29 12:58:43 -0700

    pivoting

--- src/components/serverside_grid.tsx
+++ src/components/serverside_grid.tsx
@@ -8,8 +8,9 @@ import { Looker40SDK } from '@looker/sdk'
 
 interface IinlineQueryResult {
   success?: boolean;
-  rows?:Record<any, any>[];
-  lastRow?: number
+  rows?:any;
+  lastRow?: number;
+  pivotFields?: any;
 }
 
 const AGGrid = (): JSX.Element => {
@@ -17,10 +18,21 @@ const AGGrid = (): JSX.Element => {
   const gridStyle = useMemo(() => ({ height: '100%', width: '100%' }), []);
   const core40SDK = getCoreSDK2<Looker40SDK>()
   const [columnDefs, setColumnDefs] = useState([
+    { field: "department", minWidth: 220, pivot: true},
     { field: "category", minWidth: 220,rowGroup: true, hide: true },
-    { field: "department", minWidth: 220,rowGroup: true, hide: true},
-    { field: "count_products" , minWidth: 220, aggFunc: 'sum'},
+    { field: "brand", minWidth: 220,rowGroup: true, hide: true },
+    // { field: "name", minWidth: 220,rowGroup: true, hide: true },
+    // { field: "created_at_year", minWidth: 220,rowGroup: true, hide: true},
+    // { field: "created_at_month", minWidth: 220,rowGroup: true, hide: true},
+    // { field: "created_at_day_of_week", minWidth: 220,rowGroup: true, hide: true},
+    { field: "count_products" , minWidth: 220, aggFunc: 'sum', filter: true},
+    { field: "total_retail_price" , minWidth: 220, hide: true, aggFunc: 'sum'},
+    { field: "total_cost" , minWidth: 220, hide: true, aggFunc: 'sum'},
   ]);
+
+  const PREFIX = 'inventory_items'
+  const VIEW = 'order_items'
+  const MODEL = '4_mile_analytics'
 
   const defaultColDef = useMemo(() => {
     return {
@@ -40,39 +52,123 @@ const AGGrid = (): JSX.Element => {
   const getLookerData = async (request: any) => {
 
     const lookerRequest = buildLookerQuery(request) 
+    console.log('looker request: ',lookerRequest)
     try {
       // debugger;
       const result = await core40SDK.ok(
         core40SDK.run_inline_query(lookerRequest)
       ) as unknown as Record<any, any>[]
-      const formattedResult = formatResult(result)
-      const finalResult: IinlineQueryResult = {
-        success: true,
-        rows: formattedResult,
-        lastRow: getLastRowIndex(request, result)
-      }
-      return finalResult;
+      console.log(result)
+      const formattedResult = formatResult(request,result)
+      console.log('formatted result: ', formattedResult)
+
+      return formattedResult;
     } catch (error) {
       console.log('Error invoking inline query', error)
     }
   }
 
-  function formatResult(data: any[]): any[] {
-    const formatted = data.map( (item) => {
-      let myObj = {}
-      Object.keys(item).map( property => {
-        Object.defineProperty(myObj, property.split('.')[1], 
-        { value: typeof item[property] === 'string'? item[property].trim(): item[property]})
+  // async function getPivotFields(request: any){
+  //   try {
+  //     const result = await core40SDK.ok(
+  //       core40SDK.run_inline_query(
+  //         {
+  //           result_format: 'json',
+  //           body: {
+  //             model: '4_mile_analytics',
+  //             view: 'order_items',
+  //             fields: ["inventory_items.department"],
+  //             limit: '500',
+  //             total: false
+  //           }
+  //         }
+  //       )
+  //     ) as unknown as Record<any, any>[]
+  //     const pivotFields: any[] = result.map( (item: any) => Object.values(item)[0]);
+  //     return pivotFields;
+  //   }catch (error) {
+  //     console.log('Error invoking inline query', error)
+  //   }
+  // }
+
+  function formatResult(request:any, raw: any): any[] {
+
+    let formatted:any;
+    let data;
+    // debugger;
+    if ( raw.pivots?.length > 0) {
+      // interpret looker pivot data to ag grid pivot format
+      data = dataTransform(raw)
+    } else {
+      data = raw.map( (item: any) => {
+        let myObj = {}
+        Object.keys(item).map( property => {
+          Object.defineProperty(myObj, property.split('.')[1], 
+          { value: typeof item[property] === 'string'? item[property].trim(): item[property]})
+        })
+        return myObj
       })
-      return myObj
-    })
+    }
+    console.log('formatted data: ', data)
+    // const pivotFields = raw.pivots? Object.keys(data[0]).map( key => key): [];
+    const pivotFields = ['Men_count_products', 'Women_count_products']
+    formatted = {
+      success: true,
+      rows: data,
+      lastRow: getLastRowIndex(request, raw),
+      pivotFields 
+    }
     return formatted
+  }
+
+  function dataTransform(data: any): any {
+    
+    const valCols = data.fields.measures.map( (measure: any) => measure.name);
+    console.log('value columns: ', valCols)
+    const final = data.data.map( (item:object) => {
+      let returnObj = {}
+      Object.keys(item).map( (field, i)=> {
+        if (!valCols.includes(field)) {
+          returnObj = {
+          ...returnObj,
+          [field.substring(field.indexOf('.')+1)]: Object.values(item)[i].value,
+          }
+        } else {
+          let metricsObj: object = {...Object.values(item)[i]}
+          let metrics = Object.keys(metricsObj).map( (metric,i) => {
+            metric = metric.split('|FIELD|').join('_')
+            field = field.substring(field.indexOf('.')+1)
+            return {
+              [`${metric}_${field}`]: Object.values(metricsObj)[i].value
+            }
+          })
+          let finalObj = {}
+          metrics.map( (item:object) => {
+            finalObj = { 
+              ...finalObj,
+              [Object.keys(item)[0]]: Object.values(item)[0]
+            }
+            return finalObj
+          })
+          // console.log('metrics: ', metrics)
+          returnObj = {
+          ...returnObj,
+          ...finalObj
+          }
+        }
+      })
+      return returnObj
+    })
+    return final;
   }
 
   const datasource = {
     getRows: async function(params:any) {
       console.log('[Datasource] - rows requested by grid: ', params.request);
-      const response = await getLookerData(params.request)
+      const response:any = await getLookerData(params.request)
+      // response.pivotFields = await getPivotFields(params.request)
+      // console.log('looker data: ', response)
+      addPivotColDefs(response, params.columnApi);
       console.log('Looker resp: ',response)
         if (response?.success) {
           // call the success callback
@@ -88,14 +184,36 @@ const AGGrid = (): JSX.Element => {
     },
   };  
 
+
+  //** Pivoting */
+
+  const addPivotColDefs = (response: IinlineQueryResult, columnApi: any) => {
+    // check if pivot colDefs already exist
+    let existingPivotColDefs = columnApi.getSecondaryColumns();
+    if (existingPivotColDefs && existingPivotColDefs.length > 0) {
+      return;
+    }
+    // create colDefs
+    let pivotColDefs = response.pivotFields?.map(function (field: any) {
+      // let headerName = field.split('_')[0];
+      return { headerName: field, field: field };
+    });
+    // supply secondary columns to the grid
+    columnApi.setSecondaryColumns(pivotColDefs);
+  }; 
+
   //** Looker Queries */
 
   function buildLookerQuery(request: any) {
+    
+    const pivots = request.pivotCols.map( (item: any) => `${PREFIX}.${item.id}`);
+    const result_format = pivots.length > 0? 'json_detail': 'json';
     const query = {
-      result_format: 'json',
+      result_format,
       body: {
-        model: '4_mile_analytics',
-        view: 'order_items',
+        model: MODEL,
+        view: VIEW,
+        pivots,
         fields: getFields(request),
         sorts: getSorting(request),
         filters:  getFilters(request),
@@ -111,14 +229,18 @@ const AGGrid = (): JSX.Element => {
     const groupKeys = request.groupKeys;
     const valueCols = request.valueCols;
 
-    console.log('request: ', request)
     if (groupKeys.length > 0) {
-      const dims = rowGroupCols.map( (item:any) => `inventory_items.${item.id}`);
+      const dims = [];
+      for (let i=0; i <= groupKeys.length; i++) {
+        dims.push(`inventory_items.${rowGroupCols[i].id}`)
+      }
+      console.log('dim fields: ', dims)
       const measures = valueCols.map( (item: any) => `inventory_items.${item.id}`); 
       console.log('fields', [...dims, ...measures]) 
       return [...dims, ...measures];
     } 
-    return [ 'inventory_items.category', 'inventory_items.count_products']
+    // TODO: These default columns can be passed down as props from a picklist of some kind
+    return [ 'inventory_items.department','inventory_items.category', 'inventory_items.count_products']
   }
 
   function getSorting(request: any) {
@@ -128,10 +250,9 @@ const AGGrid = (): JSX.Element => {
 
   function getFilters( request: any) {
     const filter:{ [key: string]: string; }= {}
-    const rowGroups = request.rowGroupCols; // filter key (the dimension/field to filter)
-    const groupKeys = request.groupKeys; // value to filter on
+    const rowGroups = request.rowGroupCols; 
+    const groupKeys = request.groupKeys; 
 
-    // I want { rowGroup1: "groupKey1", rowGroup2: "groupKey2" }
     if (groupKeys.length > 0) {
       groupKeys.forEach(function (key:string, i: number) {
         filter[`inventory_items.${rowGroups[i].id}`] = key
@@ -139,7 +260,6 @@ const AGGrid = (): JSX.Element => {
     }
     return filter
   }
-
 
   //** End Queries */
 
@@ -168,6 +288,8 @@ const AGGrid = (): JSX.Element => {
           serverSideStoreType={'full'}
           onGridReady={onGridReady}
           autoGroupColumnDef={autoGroupColumnDef}
+          animateRows={true}
+          pivotMode={true}
         ></AgGridReact>
       </div>
     </div>
